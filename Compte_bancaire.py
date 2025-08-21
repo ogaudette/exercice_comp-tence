402ce291a9fbc998792c928710b8e31a49e6bbaf
6331477 <6331477@cstjean.qc.ca>
2025-08-21 14:47:19 -0400

    v2

--- Compte_bancaire.py
+++ Compte_bancaire.py
@@ -1,9 +1,9 @@
 class CompteBancaire:
-    def __init__(self, titulaire, numero_compte, solde, historique):
+    def __init__(self, titulaire, numero_compte, solde_initial=0):
         self.__titulaire = titulaire
         self.__numero_compte = numero_compte
-        self.__solde = solde
-        self.__historique = historique
+        self.__solde = solde_initial
+        self.__historique = []
 
     @property
     def titulaire(self):
@@ -21,6 +21,15 @@ class CompteBancaire:
     def historique(self):
         return str(self.__historique)
 
+    @solde.setter
+    def solde(self, montant):
+        if not isinstance(montant, (int, float)):
+            raise TypeError("solde pas nombre")
+        if montant < 0:
+            raise ValueError("solde négatif")
+        self.__solde = montant
+        self.__historique.append(f"modification solde: {montant}")
+
     @numero_compte.setter
     def nouv_numero_compte(self, nouv_numero_compte):
         if nouv_numero_compte >= 0:
@@ -28,20 +37,28 @@ class CompteBancaire:
         self.__numero_compte = nouv_numero_compte
 
     def deposer(self, montant):
-        if montant > 0:
-            self.__solde += montant
-        self.__historique = f"Dépôt: +{montant}, Solde: {self.__solde}"
+        if not isinstance(montant, (int, float)):
+            raise TypeError("montant doit être un nombre")
+        if montant < 0:
+            raise ValueError("montant négatif")
+        self.__solde += montant
+        self.__historique.append(f"Dépôt: +{montant}, Solde: {self.__solde}")
 
     def retirer(self, montant):
-        if self.__solde >= montant > 0:
-            self.__solde -= montant
-        return f"Retrait : -{montant}, Solde: {self.__solde}"
+        if not isinstance(montant, (int, float)):
+            raise TypeError("montant doit être un nombre")
+        if montant < 0:
+            raise ValueError("montant négatif")
+        if montant > self.__solde:
+            raise ValueError("Solde insuffisant")
+        self.__solde -= montant
+        self.__historique.append(f"Retrait : -{montant}, Solde: {self.__solde}")
 
     def __str__(self):
         return (f"Le propriétaire du Compte Bancaire numéro {self.__numero_compte} est {self.__titulaire}."
                 f" Le solde actuel est de {self.__solde}$.")
 
-compte1 = CompteBancaire("Jo", "449", 1400, [])
+compte1 = CompteBancaire("Jo", "449", 1400)
 compte1.retirer(500)
 
 print(compte1.historique)
